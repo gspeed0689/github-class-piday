0923c4df883d9c481b6e1652f06ecb1f337dce49
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2024-06-06 13:33:46 +0200

    Add Docs to getDomains and add power

--- basic_code.py
+++ basic_code.py
@@ -1,7 +1,19 @@
-def get_domains(domain_string):
+def getDomains(domain_string):
+    
+    """This function should work, but wtf why not
+
+    Args:
+        domain_string (string): any url string
+    """
     s = str(domain_string)
     L = s.split(".")
     print(L)
     return(L)
 
-get_domains("geo-data-support.sites.uu.nl")
+def power(x, y):
+    rv = x
+    for i in range(y):
+        rv = rv * x
+    return(rv)
+
+getDomains("https://geo-data-support.sites.uu.nl")
